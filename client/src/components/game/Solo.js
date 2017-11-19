f95ff98c1f903e6c0d6f5ef0056d1b3557596d4c
Akshith Yellapragada <ayellapragada@gmail.com>
2017-11-18 19:22:36 -0500

    SessionID reset to 0 if this is a solo game.

--- client/src/components/game/Solo.js
+++ client/src/components/game/Solo.js
@@ -3,6 +3,8 @@ import React from 'react';
 import GameStart from './GameStart';
 
 const Solo = (props) => {
+  sessionStorage.setItem('id', 0);
+
   return (
     <div>
       <GameStart solo one={0} two={'HAL'} />
